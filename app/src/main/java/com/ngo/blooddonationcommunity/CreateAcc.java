4baf6a77c66af7a07027f2cda9a0218e85947c52
Ramyashree shetty <ramyashreeshetty18042000@gmail.com>
2021-12-03 15:31:28 +0530

    Initial commit

---
+++ app/src/main/java/com/ngo/blooddonationcommunity/CreateAcc.java
@@ -0,0 +1,30 @@
+package com.ngo.blooddonationcommunity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class CreateAcc extends AppCompatActivity {
+
+    TextView createnextbtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_acc);
+
+        createnextbtn=findViewById(R.id.createnextbtn);
+
+        createnextbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(CreateAcc.this,VerifyOTP.class);
+                startActivity(i);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/ngo/blooddonationcommunity/WelcomePage.java
@@ -0,0 +1,30 @@
+package com.ngo.blooddonationcommunity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class WelcomePage extends AppCompatActivity {
+
+    TextView welcomebtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome_page);
+
+        welcomebtn=findViewById(R.id.welcomebtn);
+
+        welcomebtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i=new Intent(WelcomePage.this,CreateAcc.class);
+                startActivity(i);
+            }
+        });
+    }
+}
